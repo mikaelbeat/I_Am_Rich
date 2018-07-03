497de9499988eeec49ef915126bc8cf7bdbdf1c6
Ryynanen, Petri <petri.ryynanen@accenture.comgit clone https://petri.ryynanen@innersource.accenture.com/scm/afhrtvkn/hrrt-test-scripts.gitgit config --global user.email petri.ryynanen@accenture.com>
2018-07-03 22:30:19 +0300

    Added background music

--- app/src/main/java/mikaelbeat/rich/MainActivity.java
+++ app/src/main/java/mikaelbeat/rich/MainActivity.java
@@ -1,13 +1,22 @@
 package mikaelbeat.rich;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mMediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.rich);
+        mMediaPlayer.start();
+
     }
 }
